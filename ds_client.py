04c4a698fddaca036e1ad67b35c21b0dc06bd6bd
Leo Chao <ychao13@uci.edu>
2024-02-14 19:32:36 -0800

    added init function in protocol

--- ds_client.py
+++ ds_client.py
@@ -4,16 +4,30 @@
 # ychao13@uci.edu 
 # 76846188
 
-def send(server:str, port:int, username:str, password:str, message:str, bio:str=None):
-  '''
-  The send function joins a ds server and sends a message, bio, or both
+import socket
 
-  :param server: The ip address for the ICS 32 DS server.
-  :param port: The port where the ICS 32 DS server is accepting connections.
-  :param username: The user name to be assigned to the message.
-  :param password: The password associated with the username.
-  :param message: The message to be sent to the server.
-  :param bio: Optional, a bio for the user.
-  '''
-  #TODO: return either True or False depending on results of required operation
-  return 
+def connect_serv(server:str, port:int) -> socket.socket:
+    try:
+        sockt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sockt.connect((server, port))
+        return sockt
+    except:
+        return None
+
+
+def send(server:str, port:int, username:str, password:str, message:str, bio:str=None):
+    '''
+    The send function joins a ds server and sends a message, bio, or both
+
+    :param server: The ip address for the ICS 32 DS server.
+    :param port: The port where the ICS 32 DS server is accepting connections.
+    :param username: The user name to be assigned to the message.
+    :param password: The password associated with the username.
+    :param message: The message to be sent to the server.
+    :param bio: Optional, a bio for the user.
+    '''
+    client = connect_serv(server, port)
+
+
+
+#TODO: return either True or False depending on results of required operation
--- ds_protocol.py
+++ ds_protocol.py
@@ -9,19 +9,23 @@ from collections import namedtuple
 
 # Namedtuple to hold the values retrieved from json messages.
 # TODO: update this named tuple to use DSP protocol keys
-DataTuple = namedtuple('DataTuple', ['foo','baz'])
 
-def extract_json(json_msg:str) -> DataTuple:
+DSPConnection = namedtuple('DSPConnection', ['socket','send', 'recv'])
+
+def extract_json(json_msg:str) -> DSPConnection:
   '''
-  Call the json.loads function on a json string and convert it to a DataTuple object
+  Call the json.loads function on a json string and convert it to a DSPConnection object
   
   TODO: replace the pseudo placeholder keys with actual DSP protocol keys
   '''
   try:
     json_obj = json.loads(json_msg)
+
     foo = json_obj['foo']
     baz = json_obj['bar']['baz']
   except json.JSONDecodeError:
     print("Json cannot be decoded.")
 
   return DataTuple(foo, baz)
+
+def to_json(DSP_obj:DSPConnection) -> bytes
