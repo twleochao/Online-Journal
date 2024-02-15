2c77b15f2551f0be65516e935d62b0bfb27b02d0
Leo Chao <ychao13@uci.edu>
2024-02-14 20:05:23 -0800

    a3 working on creating socket

--- a3.py
+++ a3.py
@@ -3,3 +3,23 @@
 # Leo Chao
 # ychao13@uci.edu 
 # 76846188
+
+HOST = "168.235.86.101"
+PORT = 3021
+
+import socket
+from ds_client import create_socket 
+from ds_protocol import init
+
+main():
+    sock = create_socket(HOST, PORT)
+    connection = init(sock)
+
+
+    
+
+if __name__ == "__main__":
+    main()
+    
+
+
